135c91f566cae958b9af16ff9a9008f1740a403e
Sigl Patrik <Sigl.Patrik@stud.vassboskovice.cz>
2021-10-19 10:20:29 +0200

    Přidejte soubory projektu.

---
+++ BMI1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BMI1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
