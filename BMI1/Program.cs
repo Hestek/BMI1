7ceb6d7a5aab45cdd74ef5734db49ef99a511f8d
Patrik Sigl <Sigl.Patrik@stud.vassboskovice.cz>
2021-10-19 11:17:14 +0200

    very cool

--- BMI1/Program.cs
+++ BMI1/Program.cs
@@ -6,7 +6,28 @@ namespace BMI1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("BMI CALCULATION");
+
+            Console.ReadLine();
+
+            double Weight;
+            double Height;
+            double count;
+
+            Console.WriteLine("Enter your Weight");
+            double.TryParse(Console.ReadLine(), out Weight);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter your Height");
+            double.TryParse(Console.ReadLine(), out Height);
+            Console.ReadLine();
+
+            count = (Weight) / (Height/100*Height/100);
+
+            Console.WriteLine();
+            Console.WriteLine($"Your BMI is{ count }");
+            Console.ReadLine();
+
         }
     }
 }
